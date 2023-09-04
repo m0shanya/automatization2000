4231767bc736b269999df5364fb9bf55903e3e57
m0shanya <funnytimka@gmail.com>
2023-09-04 11:01:38 +0300

    some changes #2;

--- crcrb/command_parser.py
+++ crcrb/command_parser.py
@@ -193,7 +193,7 @@ def get_min30_data(comma, insertion):
     if month > 10:
         date = f'{year}-{month}-{day} 00:00:00'
     else:
-        date = f'{year}-0{month - 1}-{day} 00:00:00'
+        date = f'{year}-0{month}-{day} 00:00:00'
 
     number = comma[11]
     query = f"""SELECT V{number} FROM L2HALF_HOURLY_ENERGY WHERE M_SWVMID={vmid} AND M_SDTDATE='{date}' AND
@@ -251,9 +251,9 @@ def get_day_data(comma, insertion):
                     day = 31 - delta
 
             if month > 10:
-                date = f'{year}-{month}-{day} 00:00:00'
+                date = f'{year}-{month}-{day - i} 00:00:00'
                 break
-            date = f'{year}-0{month}-{day} 00:00:00'
+            date = f'{year}-0{month}-{day - i} 00:00:00'
             break
 
     query = f"""SELECT M_SFVALUE FROM L3ARCHDATA WHERE M_SWVMID={vmid} AND M_STIME='{date}' AND
