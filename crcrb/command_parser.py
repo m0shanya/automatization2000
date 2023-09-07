aac26ca7298c3640c70730727337892d6b56ae8e
m0shanya <funnytimka@gmail.com>
2023-09-07 17:03:30 +0300

    need to set values for date in 0x85 command;

--- crcrb/command_parser.py
+++ crcrb/command_parser.py
@@ -4,6 +4,7 @@ import os
 import socket
 
 from datetime import datetime
+from copy import deepcopy
 from crc16ccc import crc16_chk, reverse_CRC16
 from dotenv import load_dotenv, find_dotenv
 
@@ -54,13 +55,13 @@ def test_cmd(choose: int):
     elif choose == 3:
         byte_cmd = [0x55, 0x81, 0x00, 0x12, 0x00, 0x42, 0x00, 0x11, 0x00, 0x04, 0x00, 0x00, 0x00, 0x04, 0x00, 0x01]
     elif choose == 4:
-        byte_cmd = [0x55, 0x81, 0x00, 0x12, 0x00, 0x52, 0x00, 0x11, 0x00, 0x04, 0x00, 0x1b, 0x00, 0x02, 0x00, 0x01]
+        byte_cmd = [0x55, 0x81, 0x00, 0x12, 0x00, 0x52, 0x00, 0x11, 0x00, 0x04, 0x00, 0x03, 0x00, 0x01, 0x00, 0x01]
     elif choose == 5:
         byte_cmd = [0x55, 0x81, 0x00, 0x12, 0x00, 0x80, 0x00, 0x11, 0x00, 0x04, 0x00, 0x01, 0x00, 0x04, 0x00, 0x01]
     elif choose == 6:
         byte_cmd = [0x55, 0x81, 0x00, 0x12, 0x00, 0x81, 0x00, 0x11, 0x00, 0x04, 0x00, 0x03, 0x00, 0x04, 0x00, 0x01]
     elif choose == 7:
-        byte_cmd = [0x55, 0x81, 0x00, 0x10, 0x00, 0x85, 0x00, 0x11, 0x00, 0x04, 0x00, 0x05, 0x00, 0x04]
+        byte_cmd = [0x55, 0x81, 0x00, 0x10, 0x00, 0x85, 0x00, 0x11, 0x00, 0x04, 0x00, 0x01, 0x00, 0x17]
     else:
         return
     byte_cmd += reverse_CRC16(crc16_chk(byte_cmd))  # составляю и добавляю ЦРЦ во всю посылку
@@ -93,35 +94,45 @@ def execute_query(query):
 
 def another_data(response, data, timer, cmd):
     response = response[0:2]
+
     if cmd[5] == 0x85:
-        response += [0x00] + [cmd[5]]
-        response.append(struct.pack("!f", timer["second"])[1])
-        response.append(struct.pack("!f", timer["minute"])[1])
-        response.append(struct.pack("!f", timer["hour"])[1])
-        response.append(struct.pack("!f", timer["day"])[1])
-        response.append(struct.pack("!f", timer["month"])[1])
-        response.append(struct.pack("!f", timer["year"])[1])
-        if data == [] or None:
-            response += [0xff] * cmd[9] * (cmd[9] // 4) * cmd[13]
-        else:
-            response += data
+        response = putting_allen_data(response, data, timer, cmd)
     else:
         if data == [] or None:
-            response += [0x00] + [cmd[5]] + [0xff] * cmd[9] * (cmd[9] // 4) * cmd[13]
+            response += [0x00] + [cmd[5]] + [0xff] * 4 * cmd[9] * (cmd[9] // 4) * cmd[13]
         else:
             response += [0x00] + [cmd[5]] + data
 
     checker = 0x00 if data != [] or None else 0x01
     response += [checker]
-    response.extend([struct.pack("!f", timer["minute"])[1],
-                     struct.pack("!f", timer["hour"])[1],
-                     struct.pack("!f", timer["day"])[1],
-                     struct.pack("!f", timer["month"])[1],
-                     struct.pack("!f", timer["year"])[1]])
+    response.extend([struct.pack("!i", timer["minute"])[-1],
+                     struct.pack("!i", timer["hour"])[-1],
+                     struct.pack("!i", timer["day"])[-1],
+                     struct.pack("!i", timer["month"])[-1],
+                     struct.pack("!i", timer["year"])[-1]])
     response.append(cmd[14])
     response.append(cmd[15])
     response.insert(2, 0)
     response.insert(3, int(hex(len(response) + 3), 16))
+    return response
+
+
+def putting_allen_data(response, data, timer, cmd):
+    response += [0x00] + [cmd[5]]
+    data_copy = deepcopy(data)
+    for i in range(0, cmd[9] * (cmd[9] // 4) * cmd[11]):
+        response.append(struct.pack("!i", timer["second"])[-1])
+        response.append(struct.pack("!i", timer["minute"])[-1])
+        response.append(struct.pack("!i", timer["hour"])[-1])
+        response.append(struct.pack("!i", timer["day"])[-1])
+        response.append(struct.pack("!i", timer["month"])[-1])
+        response.append(struct.pack("!i", timer["year"])[-1])
+        if data == [] or None:
+            response += [0xff] * 4
+        else:
+            response += data_copy[0:4]
+            data_copy = data_copy[4:len(data_copy)]
+
     return response
 
 
@@ -210,12 +221,12 @@ def get_min30_data(comma, insertion):
         date = f'{year}-0{month}-{day} 00:00:00'
 
     if comma[13] == 1:
-        numbers["values"] = f"V{comma[11]}"
+        numbers = f"V{comma[11]}"
     else:
         for i in range(comma[11], comma[11] + comma[13]):
             numbers += f"V{i}, "
 
-    numbers = numbers[0:len(numbers) - 2]
+        numbers = numbers[0:len(numbers) - 2]
 
     query = f"""SELECT {numbers} FROM L2HALF_HOURLY_ENERGY WHERE M_SWVMID={vmid} AND M_SDTDATE='{date}' AND
      M_SWCMDID BETWEEN 13 AND 16"""
@@ -302,17 +313,22 @@ def get_allen_data(comma, insertion):
     minute = timer["minute"]
     second = timer["second"]
 
-    if month >= 10:
-        date = f'{year}-{month}-{day} {hour}:{minute}:{second}'
-    elif month > 10 and day < 10:
-        date = f'{year}-{month}-0{day} {hour}:{minute}:{second}'
-    elif month < 10 and day < 10:
-        date = f'{year}-0{month}-0{day} {hour}:{minute}:{second}'
-    else:
-        date = f'{year}-0{month}-{day} {hour}:{minute}:{second}'
+    # if month >= 10:
+    #     date = f'{year}-{month}-{day} {hour}:{minute}:{second}'
+    # elif month > 10 and day < 10:
+    #     date = f'{year}-{month}-0{day} {hour}:{minute}:{second}'
+    # elif month < 10 and day < 10:
+    #     date = f'{year}-0{month}-0{day} {hour}:{minute}:{second}'
+    # else:
+    #     date = f'{year}-0{month}-{day} {hour}:{minute}:{second}'
+    if comma[10] != 0:
+        query = f"""SELECT M_SFVALUE, cast (M_STIME as date) FROM L3CURRENTDATA WHERE M_SWVMID={vmid} AND cast (M_STIME as date)='2023-08-29' AND
+        M_SWTID BETWEEN {comma[10]} and {comma[11]} AND M_SWCMDID BETWEEN 1 AND 4 ORDER BY M_SWCMDID"""
 
-    query = f"""SELECT M_SFVALUE FROM L3CURRENTDATA WHERE M_SWVMID={vmid} AND M_STIME='{date}' AND
-    M_SWTID BETWEEN {comma[12]} and {comma[13] - 1} AND M_SWCMDID BETWEEN 1 AND 4 ORDER BY M_SWCMDID"""
+        return execute_query(query)
+
+    query = f"""SELECT M_SFVALUE, cast (M_STIME as date) FROM L3CURRENTDATA WHERE M_SWVMID={vmid} AND cast (M_STIME as date)='2023-08-29' AND
+            M_SWTID={comma[10]} AND M_SWCMDID BETWEEN 1 AND 4 ORDER BY M_SWCMDID"""
 
     return execute_query(query)
 
@@ -383,24 +399,24 @@ def get_response(cmd):
 
 
 if __name__ == "__main__":
-    # get_response(test_cmd(7))
-    try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        server.bind(("0.0.0.0", 5019))
-
-        server.listen(500)
-
-        while True:
-            client, address = server.accept()
-            print(address)
-            response = client.recv(1024)
-            print(response.hex())
-            command = []
-            for i in range(0, len(response), 1):
-                value = response[i:i + 1].hex()
-                command.append(int('0x' + value, 16))
-            print(command)
-            client.send(bytes(get_response(command)))
-    except KeyboardInterrupt as e:
-        print(e)
+    get_response(test_cmd(7))
+    # try:
+    #     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #
+    #     server.bind(("0.0.0.0", 5009))
+    #
+    #     server.listen(500)
+    #
+    #     while True:
+    #         client, address = server.accept()
+    #         print(address)
+    #         response = client.recv(1024)
+    #         print(response.hex())
+    #         command = []
+    #         for i in range(0, len(response), 1):
+    #             value = response[i:i + 1].hex()
+    #             command.append(int('0x' + value, 16))
+    #         print(command)
+    #         client.send(bytes(get_response(command)))
+    # except Exception as e:
+    #     print(e)
